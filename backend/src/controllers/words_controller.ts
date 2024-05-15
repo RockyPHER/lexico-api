faf141fc13ca60f3bb4e5492a63785ef54bd376d
RockyPHER <davidsonjunior10@gmail.com>
2024-05-15 19:17:44 -0300

    refactor: GetWordController try catch add

--- backend/src/controllers/words_controller.ts
+++ backend/src/controllers/words_controller.ts
@@ -3,10 +3,20 @@ import { AddWord, DeleteWord, GetWord, GetWords, UpdateWord } from "../services/
 
 
 export async function GetWordController(req: Request, res: Response) {
-    const name = req.params.id;
-    console.log(name);
-    const words = await GetWord(name);
-    res.json(words);
+    try {
+        const name = req.params.name;
+        if (name === "" || name === null) throw new Error("InvalidWordInput");
+
+        const word = await GetWord(name);
+        if (word === null) throw new Error("WordNotFound");
+
+        res.status(201).json(word);
+    } catch (err: any) {
+        if (err.message === "InvalidWordInput") return res.status(400).json(err);
+        if (err.message === "WordNotFound") return res.status(404).json(err);
+
+        return res.status(500).json(err);
+    }
 }
 
 export async function GetWordsController(req: Request, res: Response) {
